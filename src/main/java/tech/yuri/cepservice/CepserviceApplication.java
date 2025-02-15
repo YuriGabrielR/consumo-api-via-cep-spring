24b46bbc3b420d593f1fc51ab48f4f66e4191a5a
Yuri Ramos <yurigabriel21@yahoo.com>
2025-02-15 11:01:24 -0300

    config: commit dos arquivos de configuração do projeto

---
+++ src/main/java/tech/yuri/cepservice/CepserviceApplication.java
@@ -0,0 +1,13 @@
+package tech.yuri.cepservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CepserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CepserviceApplication.class, args);
+	}
+
+}
